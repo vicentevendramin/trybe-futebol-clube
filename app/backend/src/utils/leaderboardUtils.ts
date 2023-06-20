f28d658ec1cdc3af9ca154b54e71153f325a79b5
Vicente Vendramin <guesser.vicente@gmail.com>
2023-06-20 17:51:26 -0300

    feat: requisito 24

--- app/backend/src/utils/leaderboardUtils.ts
+++ app/backend/src/utils/leaderboardUtils.ts
@@ -10,9 +10,9 @@ const queriesLB: IQueryLB = {
   q5: 'CAST(SUM(CASE WHEN home_team_goals > 0 THEN home_team_goals ELSE 0 END) AS UNSIGNED)',
   q6: 'CAST(SUM(CASE WHEN away_team_goals > 0 THEN away_team_goals ELSE 0 END) AS UNSIGNED)',
   q7: 'SUM(home_team_goals - away_team_goals)',
-  q8: `ROUND(SUM(CASE WHEN home_team_goals > away_team_goals THEN 3
+  q8: `ROUND((SUM(CASE WHEN home_team_goals > away_team_goals THEN 3
     WHEN home_team_goals = away_team_goals THEN 1
-    ELSE 0 END))`,
+    ELSE 0 END) / (COUNT(home_team_id) * 3)) * 100, 2)`,
   ids: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
 };
 
@@ -27,6 +27,8 @@ const sequelizeAtt = [
   [literal(queriesLB.q4), 'totalLosses'],
   [literal(queriesLB.q5), 'goalsFavor'],
   [literal(queriesLB.q6), 'goalsOwn'],
+  [literal(queriesLB.q7), 'goalsBalance'],
+  [literal(queriesLB.q8), 'efficiency'],
 ] as unknown as ProjectionAlias[];
 
 interface ISortLB {
